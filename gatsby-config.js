26ebe8a86856c7d6fc8ef11260ebf745dc93d297
matt <matt.pate@finimize.com>
2019-12-02 21:11:13 +0000

    add dato source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
                     }
                 ]
             }
-        }
+        },
         // {
         //     resolve: `gatsby-plugin-favicon`,
         //     options: {
